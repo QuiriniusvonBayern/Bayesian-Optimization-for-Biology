6e02dd074f50864e6096e84f82d786b3343a3215
Lasse Wriedt <lasse.wriedt@stud.th-luebeck.de>
2026-06-30 10:01:09 +0200

    Add support for Pareto front optimization in GP model training
    - Introduced a new parameter `use_pareto_front` in `train_gp_model` to enable Pareto front optimization.
    - Implemented `train_with_pareto_front` function to create a `ModelListGP` with two sub-models for the objectives.
    - Updated the model training process to optimize hyperparameters for each sub-model when using Pareto front.
    - Ensured compatibility with single and multi-dimensional outputs for training data.

--- src/bay_op_projekt/model_training.py
+++ src/bay_op_projekt/model_training.py
@@ -1,18 +1,48 @@
 from botorch.models import SingleTaskGP
 from botorch.fit import fit_gpytorch_mll
 from gpytorch.mlls import ExactMarginalLogLikelihood
+from botorch.models.model_list_gp_regression import ModelListGP
+import torch
 
-def train_gp_model(train_X, train_Y, train_Yvar):
+def train_gp_model(train_X, train_Y, train_Yvar, use_pareto_front:bool=False):
     # GP-Modell erstellen
-    model = SingleTaskGP(
-        train_X=train_X,
-        train_Y=train_Y,
-        train_Yvar=train_Yvar
-    )
 
-    # Hyperparameter des GP optimieren
-    mll = ExactMarginalLogLikelihood(model.likelihood, model)
-    fit_gpytorch_mll(mll)
+    if use_pareto_front:
+        model = train_with_pareto_front(train_X, train_Y, train_Yvar)
 
-    model.eval()
+        # For ModelListGP, optimize each sub-model separately
+        for m in model.models:
+            mll = ExactMarginalLogLikelihood(m.likelihood, m)
+            fit_gpytorch_mll(mll)
+
+        model.eval()
+    else:
+        model = SingleTaskGP(
+            train_X=train_X,
+            train_Y=train_Y,
+            train_Yvar=train_Yvar
+        )
+
+        # Hyperparameter des GP optimieren
+        mll = ExactMarginalLogLikelihood(model.likelihood, model)
+        fit_gpytorch_mll(mll)
+        model.eval()
+
+    return model
+
+def train_with_pareto_front(train_X, train_Y, train_Yvar) -> ModelListGP:
+
+    if train_Y.dim() == 1:
+        train_Y = train_Y.unsqueeze(-1)
+    if train_Yvar.dim() == 1:
+        train_Yvar = train_Yvar.unsqueeze(-1)
+
+    train_Y1 = torch.tensor(train_Y, dtype=torch.double)
+    train_Y2 = torch.tensor(-train_Yvar, dtype=torch.double)
+
+    model1 = SingleTaskGP(train_X, train_Y1)
+    model2 = SingleTaskGP(train_X, train_Y2)
+
+    model  = ModelListGP(model1, model2)
+
     return model
